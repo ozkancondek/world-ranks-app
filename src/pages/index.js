89309291b6c90ca1322a9721fe1752bea1668a0a
ozkancondek <ozkncndek@gmail.com>
2022-05-11 16:27:59 +0200

    3:get data with static props

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,18 @@ import Image from "next/image";
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ countries }) {
+  console.log(countries);
   return <Layout>main</Layout>;
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.com/v2/regionalbloc/eu");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
